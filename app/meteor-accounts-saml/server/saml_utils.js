4dc848ecfaaa9fd8eb86b3cb8c0d58d6d55a0f1e
Marco Schmid <marco@marcoschmid.me>
2019-06-27 13:07:36 +0200

    fixed again

--- app/meteor-accounts-saml/server/saml_utils.js
+++ app/meteor-accounts-saml/server/saml_utils.js
@@ -97,10 +97,9 @@ SAML.prototype.generateAuthorizeRequest = function(req) {
 	}
 
 	request
-		+= '<samlp:RequestedAuthnContext xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" Comparison="exact">'
+		// += '<samlp:RequestedAuthnContext xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" Comparison="exact">'
 		// + '<saml:AuthnContextClassRef xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport</saml:AuthnContextClassRef></samlp:RequestedAuthnContext>\n'
-		+ '</samlp:RequestedAuthnContext>\n'
-		+ '</samlp:AuthnRequest>';
+		+= '</samlp:AuthnRequest>';
 
 	return request;
 };
